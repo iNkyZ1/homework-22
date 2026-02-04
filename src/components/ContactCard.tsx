a1ab50cb4e3a5ca077b7c2c6e12f33c049034e7e
Nick <n.t.1996@mail.ru>
2026-02-05 00:04:13 +0400

    refactor(ui): move ContactCard to components and fix ContactPage

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -1,33 +1,37 @@
-import React, {memo} from 'react';
-import {ContactDto} from 'src/types/dto/ContactDto';
-import {Card, ListGroup} from 'react-bootstrap';
-import {Link} from 'react-router-dom';
+import React, { memo } from "react";
+import { Card, ListGroup } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+import type { Contact } from "src/entities/contact/model/types";
+import { FavoriteToggleButton } from "src/features/favorites/model/ui/FavoriteToggleButton";
 
 interface ContactCardProps {
-  contact: ContactDto,
-  withLink?: boolean
+  contact: Contact;
+  withLink?: boolean;
 }
 
-export const ContactCard = memo<ContactCardProps>(({
-    contact: {
-      photo,
-      id,
-      name,
-      phone,
-      birthday,
-      address
-    }, withLink
-  }) => {
+export const ContactCard = memo<ContactCardProps>(
+  ({ contact: { photo, id, name, phone, birthday, address }, withLink }) => {
     return (
-      <Card key={id}>
+      <Card>
         <Card.Img variant="top" src={photo} />
+
         <Card.Body>
-          <Card.Title>
-            {withLink ? <Link to={`/contact/${id}`}>{name}</Link> : name}
+          <Card.Title className="d-flex justify-content-between align-items-center">
+            <span>
+              {withLink ? <Link to={`/contacts/${id}`}>{name}</Link> : name}
+            </span>
+
+            <FavoriteToggleButton contactId={id} />
           </Card.Title>
+
           <Card.Body>
             <ListGroup>
-              <ListGroup.Item><Link to={`tel:${phone}`} target="_blank">{phone}</Link></ListGroup.Item>
+              <ListGroup.Item>
+                <Link to={`tel:${phone}`} target="_blank">
+                  {phone}
+                </Link>
+              </ListGroup.Item>
               <ListGroup.Item>{birthday}</ListGroup.Item>
               <ListGroup.Item>{address}</ListGroup.Item>
             </ListGroup>
@@ -35,5 +39,5 @@ export const ContactCard = memo<ContactCardProps>(({
         </Card.Body>
       </Card>
     );
-  }
-)
+  },
+);
--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,49 +1,9 @@
-import React, { memo } from "react";
-import { Card, ListGroup } from "react-bootstrap";
-import { Link, useParams } from "react-router-dom";
+import React from "react";
+import { useParams } from "react-router-dom";
 
-import type { Contact } from "src/entities/contact/model/types";
-import { FavoriteToggleButton } from "src/features/favorites/model/ui/FavoriteToggleButton";
-
+import { ContactCard } from "src/components/ContactCard";
 import { useAppSelector } from "src/shared/lib/storeHooks";
 import { selectContactById } from "src/entities/contact/model/selectors";
-
-interface ContactCardProps {
-  contact: Contact;
-  withLink?: boolean;
-}
-
-export const ContactCard = memo<ContactCardProps>(
-  ({ contact: { photo, id, name, phone, birthday, address }, withLink }) => {
-    return (
-      <Card>
-        <Card.Img variant="top" src={photo} />
-
-        <Card.Body>
-          <Card.Title className="d-flex justify-content-between align-items-center">
-            <span>
-              {withLink ? <Link to={`/contacts/${id}`}>{name}</Link> : name}
-            </span>
-
-            <FavoriteToggleButton contactId={id} />
-          </Card.Title>
-
-          <Card.Body>
-            <ListGroup>
-              <ListGroup.Item>
-                <Link to={`tel:${phone}`} target="_blank">
-                  {phone}
-                </Link>
-              </ListGroup.Item>
-              <ListGroup.Item>{birthday}</ListGroup.Item>
-              <ListGroup.Item>{address}</ListGroup.Item>
-            </ListGroup>
-          </Card.Body>
-        </Card.Body>
-      </Card>
-    );
-  },
-);
 
 export function ContactPage() {
   const { contactId } = useParams<{ contactId: string }>();
