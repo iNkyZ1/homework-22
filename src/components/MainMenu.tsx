c19b8a5170c15e32fa20a6dddd4056c2c1d9a86c
Nick <n.t.1996@mail.ru>
2026-02-05 00:23:05 +0400

    fix(navigation): correct favorites route in main menu

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import {Container, Nav, Navbar} from 'react-bootstrap';
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 export const MainMenu = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="/" ><h1>Книга контактов</h1></Navbar.Brand>
+        <Navbar.Brand href="/">
+          <h1>Книга контактов</h1>
+        </Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="/groups">Группы</Nav.Link>
-          <Nav.Link href="/favorit">Избранное</Nav.Link>
+          <Nav.Link href="/favorites">Избранное</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
   );
-}
+};
